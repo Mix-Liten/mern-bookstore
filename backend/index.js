55c9c0f8767f8bc3df975be40895113fdedc508c
Mix-Liten <mixliten@gmail.com>
2023-09-19 17:11:14 +0800

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 import booksRoute from './routes/bookRoute'
 import { PORT, mongoDBURL } from './config'
@@ -9,6 +10,19 @@ import { errorHandling } from './middlewares/errorHandling'
 const app = express()
 
 app.use(express.json())
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors())
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: ['http://localhost:3000'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
+//   }),
+// )
 
 app.get('/', (req, res) => {
   return res.status(234).send('Welcome To BookStore Backend API')
